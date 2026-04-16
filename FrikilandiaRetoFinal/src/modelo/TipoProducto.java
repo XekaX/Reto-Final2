4c5a6042ea213a63887e8df0f635522ff70b305b
Andertxu11 <anderarri10@gmail.com>
2026-04-16 09:47:42 +0200

    Delete FrikilandiaRetoFinal directory

--- FrikilandiaRetoFinal/src/modelo/TipoProducto.java
+++
@@ -1,5 +0,0 @@
-package modelo;
-
-public enum TipoProducto {
-	CARTAS, COMICS;
-}
--- FrikilandiaRetoFinal/src/utilidades/InterfazExportadorProductos.java
+++
@@ -1,8 +0,0 @@
-package utilidades;
-
-import java.util.List;
-import modelo.Producto;
-
-public interface InterfazExportadorProductos {
-    void exportarProductos(List<Producto> listaProductos, String ruta);
-}
