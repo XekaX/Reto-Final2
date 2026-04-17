22214c7161485447ec0fbc378c6c1c45600c5f8a
Andertxu11 <anderarri10@gmail.com>
2026-04-17 08:49:48 +0200

    Delete src directory

--- src/util/InterfazExportadorProductos.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-import java.util.List;
-import modelo.Producto;
-
-public interface InterfazExportadorProductos {
-    void exportarProductos(List<Producto> listaProductos, String ruta);
-}
